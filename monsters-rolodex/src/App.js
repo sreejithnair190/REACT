b72a21df0c54068f55ac700a71a407ea813951a2
sreejith-nair-zedex <shreejith.nair@zedexinfo.com>
2023-04-24 22:45:26 +0530

    monster rolodex

---
+++ monsters-rolodex/src/App.js
@@ -0,0 +1,32 @@
+import { Component } from 'react';
+
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+
+  constructor() {
+    super();
+
+    this.state = {
+      name:'Sreejith'
+    }
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Hi {this.state.name}
+          </p>
+          <button>Change Name</button>
+        </header>
+      </div>
+    );
+  }
+  
+}
+
+export default App;
